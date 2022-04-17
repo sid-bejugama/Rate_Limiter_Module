79067c646f6bb6fb78ec5df9e337d26220ae7090
sb0231 <sid.bejugama@gmail.com>
2022-04-16 22:27:11 -0400

    RateLimiterWork

--- rateLimiter.py
+++ rateLimiter.py
@@ -42,20 +42,24 @@ def _getConnection():
 
 # adds a new IP address to the database
 def _addIPAddress(IPAddress, cur, userID = None):
-    postgreSQL_insert_Query = f""" INSERT INTO {IP_table} ({IP_addresses, IP_requests, IP_start_requests, IP_users}) VALUES (%s,%s,%s,%s)"""
     if userID:
-        cur.execute(postgreSQL_insert_Query, (IPAddress, 0, datetime.now(), [userID]))
+        postgreSQL_insert_Query = f""" INSERT INTO {IP_table} ({IP_addresses, IP_requests, IP_start_requests, IP_users}) VALUES ({IPAddress}, {0}, {datetime.now()}, {[userID]})"""
+        
+        cur.execute(postgreSQL_insert_Query)
     else:
-        cur.execute(postgreSQL_insert_Query, (IPAddress, 0, datetime.now(), []))
+        postgreSQL_insert_Query = f""" INSERT INTO {IP_table} ({IP_addresses, IP_requests, IP_start_requests, IP_users}) VALUES ({IPAddress}, {0}, {datetime.now()}, {[]})"""
+        
+        cur.execute(postgreSQL_insert_Query)
 
 # adds a new user to the database and updates the users associated with a given IP address
 def _addUser(userID, IPAddress, IP_info, cur):
-    postgreSQL_update_query = f"""Update {IP_table} set {IP_users} = %s where {IP_addresses} = %s"""
     new_IP_info = IP_info[0][f"{userID}"].append(userID)
-    cur.execute(postgreSQL_update_query, (new_IP_info, IPAddress))
+    postgreSQL_update_query = f"""Update {IP_table} set {IP_users} = {new_IP_info} where {IP_addresses} = {IPAddress}"""
+    
+    cur.execute(postgreSQL_update_query)
 
-    postgreSQL_insert_Query = f""" INSERT INTO {userID_table} ({userIDs}, {user_requests}, {user_start_requests}) VALUES (%s,%s,%s)"""
-    cur.execute(postgreSQL_insert_Query, (userID, 1, datetime.now()))
+    postgreSQL_insert_Query = f""" INSERT INTO {userID_table} ({userIDs}, {user_requests}, {user_start_requests}) VALUES ({userID}, {1}, {datetime.now()})"""
+    cur.execute(postgreSQL_insert_Query)
 
 # monitors the api usage for a given IP address and userID. Returns True if a given IP address and/or userID is allowed to use the api in the current time interval and returns False otherwise based on the parameters set
 def track_api_usage(userID = None):
@@ -68,8 +72,8 @@ def track_api_usage(userID = None):
     cur = connection.cursor()
 
     # obtain information corresponding to given IP address
-    postgreSQL_select_Query = f"select * from {IP_table} where {IP_addresses} = %s"
-    cur.execute(postgreSQL_select_Query, (IPAddress, ))
+    postgreSQL_select_Query = f"select * from {IP_table} where {IP_addresses} = {IPAddress}"
+    cur.execute(postgreSQL_select_Query)
     IP_info = cur.fetchall()
 
     # add a new IP address if given IP address was not found
@@ -85,21 +89,19 @@ def track_api_usage(userID = None):
     
     # reset the starting time for requests for the given IP address and reset the current number of requests in the interval if time interval exceeded
     if curr_IP_interval > limit_interval:
-        postgreSQL_update_query = f"""Update {IP_table} set {IP_start_requests} = %s, {IP_requests} = %s where {IP_addresses} = %s"""
-        
-        cur.execute(postgreSQL_update_query, (datetime.now(), 0, IPAddress))
+        postgreSQL_update_query = f"""Update {IP_table} set {IP_start_requests} = {datetime.now()}, {IP_requests} = {0}where {IP_addresses} = {IPAddress}"""
     
     # increment the number of requests
-    postgreSQL_update_query = f"""Update {IP_table} set {IP_requests} = %s where {IP_addresses} = %s""" 
-    cur.execute(postgreSQL_update_query, (IP_info[0][f"{IP_requests}"] + 1, IPAddress))
+    postgreSQL_update_query = f"""Update {IP_table} set {IP_requests} = {IP_info[0][f"{IP_requests}"] + 1} where {IP_addresses} = {IPAddress}""" 
+    cur.execute(postgreSQL_update_query)
 
     # protocol if optional userID is passed in
     if userID:
         # protocol for if the userID is already registered for the given IP address
         if userID in IP_info[0][f"{userID}"]:
                 # get user-specific information
-                postgreSQL_select_Query = f"select * from {userID_table} where {userIDs} = %s"
-                cur.execute(postgreSQL_select_Query, (userID, ))
+                postgreSQL_select_Query = f"select * from {userID_table} where {userIDs} = {userID}"
+                cur.execute(postgreSQL_select_Query)
                 user_info = cur.fetchall()
 
                 # check whether the user has exceeded the user-specific rate limit
@@ -109,13 +111,13 @@ def track_api_usage(userID = None):
                 
                  # reset the starting time for requests for the given userID and reset the current number of requests in the interval if time interval exceeded
                 if curr_user_interval > limit_interval:
-                    postgreSQL_update_query = f"""Update {userID_table} set {user_start_requests} = %s, {user_requests} = %s where {userIDs} = %s"""
+                    postgreSQL_update_query = f"""Update {userID_table} set {user_start_requests} = {datetime.now()}, {user_requests} = {0} where {userIDs} = {userID}"""
 
-                    cur.execute(postgreSQL_update_query, (datetime.now(), 0, userID))
+                    cur.execute(postgreSQL_update_query)
                 
                 # increment the number of requests for the user 
-                postgreSQL_update_query = f"""Update {userID_table} set {user_requests} = %s where {userIDs} = %s"""
-                cur.execute(postgreSQL_update_query, (user_info[0][f"{user_requests}"] + 1, userID))
+                postgreSQL_update_query = f"""Update {userID_table} set {user_requests} = {user_info[0][f"{user_requests}"] + 1} where {userIDs} = {userID}"""
+                cur.execute(postgreSQL_update_query)
         else:
             # if the given user is not found in the list of users, create a new user
             _addUser(userID, IPAddress, IP_info, cur)
@@ -148,8 +150,8 @@ def tracked_usage():
         allInfoHash[row[f"{IP_addresses}"]] = row[f"{IP_requests}"]
         # for each user associated with a given IP address, record current number of requests
         for userID in row[3]:
-            postgreSQL_select_Query = f"select * from {userID_table} where {userIDs} = %s"
-            cur.execute(postgreSQL_select_Query, (userID, ))
+            postgreSQL_select_Query = f"select * from {userID_table} where {userIDs} = {userID}"
+            cur.execute(postgreSQL_select_Query)
             userID_info = cur.fetchall()
             allInfoHash[userID_info[0][f"{userIDs}"]] = userID_info[0][f"{user_requests}"] 
     
